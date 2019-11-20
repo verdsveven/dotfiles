e1cb79c83ed8441f75ae8b7d0d60b0d9b403aa96
Lukasz M <ttecluk@gmail.com>
2019-11-20 19:30:17 +0100

    revised the autostart.sh script for dwm; new features and simpler structure for the xsetroot

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -6,6 +6,12 @@ amixer set Master 0
 picom & disown
 
 while true; do
-	xsetroot -name "$(echo $(echo"|"; iw dev wlp8s0 link | grep -i SSID; echo "|"; echo bat:; cat /sys/class/power_supply/BAT0/capacity; echo %; echo "|"; echo "vol:"; amixer get Master | grep -o --max-count=1 "[0-9]*%"; echo "|"; date))"
+	xsetroot -name "$(echo $(echo "|";echo "wifi: `iw dev wlp8s0 link | grep -i SSID | sed -n -e 's/^.*SSID: //p'` |"; 
+	echo "bat: `cat /sys/class/power_supply/BAT0/capacity`%, `cat /sys/class/power_supply/BAT0/status`"; 
+	echo "| vol:"; amixer get Master | grep -o --max-count=1 "[0-9]*%"; 
+	echo "| lang:"; 
+	cat /etc/vconsole.conf | grep -o "[a-z][a-z]"; 
+	echo "|"; 
+	date))"
 	sleep 1
 done & disown 
